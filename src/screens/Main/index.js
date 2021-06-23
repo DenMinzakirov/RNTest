1f576c582e6b57bc3f998c5bdc0856f7e3da37f3
DenMinzakirov <negranit8@gmail.com>
2021-06-23 13:04:36 +0300

    add: main screen

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
+import { Input, Button } from 'react-native-elements';
 
 const Main = () => {
   return (
     <SafeAreaView>
-      <View style={{ height: 200, backgroundColor: 'yellow', width: 200 }}>
-        <Text>Main</Text>
+      <View
+        style={{
+          height: '100%',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'red',
+        }}
+      >
+        <Text
+          style={{
+            backgroundColor: 'yellow',
+            textAlign: 'center',
+            marginVertical: 20,
+          }}
+        >
+          Hello!
+        </Text>
+        <Text
+          style={{
+            backgroundColor: 'yellow',
+            textAlign: 'center',
+            marginHorizontal: 20,
+            marginVertical: 20,
+          }}
+        >
+          Это прилажение поможет подобрать фильмы к просмотру
+        </Text>
+        <Input
+          placeholder='Введите название'
+          containerStyle={{ width: '80%', marginVertical: 20 }}
+        />
+        <Button
+          title='Найти'
+          containerStyle={{ width: '80%', marginVertical: 20 }}
+        />
       </View>
     </SafeAreaView>
   );
