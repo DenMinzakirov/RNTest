794b786aab73ee540aa35909595c15ac7cd5039f
DenMinzakirov <negranit8@gmail.com>
2021-06-23 12:03:53 +0300

    add: tab navigation

---
+++ src/screens/List/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const List = () => {
+  return (
+    <SafeAreaView>
+      <View style={{ height: 200, backgroundColor: 'yellow', width: 200 }}>
+        <Text>List</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default List;
---
+++ src/screens/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const Main = () => {
+  return (
+    <SafeAreaView>
+      <View style={{ height: 200, backgroundColor: 'yellow', width: 200 }}>
+        <Text>Main</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Main;
