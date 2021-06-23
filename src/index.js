894e2b1d1acd4f087f9ef7669361fe2a9071e220
DenMinzakirov <negranit8@gmail.com>
2021-06-23 15:19:18 +0300

    add: redux + config store

--- src/index.js
+++ src/index.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
 import Main from './screens/Main';
 import List from './screens/List';
+import {store} from './redux/store';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 export default App = () => {
+    console.log('store',store);
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='Main' component={Main} />
-        <Tab.Screen name='List' component={List} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name='Main' component={Main} />
+          <Tab.Screen name='List' component={List} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
